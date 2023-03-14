aaa989647422a974906da0dd0382f22dd2ed3cea
AilsaYang <ailsayang831031@gmail.com>
2023-03-14 12:46:52 +0800

    Initialize project

---
+++ app/js/script.js
@@ -0,0 +1,54 @@
+// 初始變數
+let list = document.querySelector("#my-todo");
+let addBtn = document.querySelector("#add-btn");
+let input = document.querySelector("#new-todo");
+
+// 資料
+const todos = [
+  "Hit the gym",
+  "Read a book",
+  "Buy eggs",
+  "Organize office",
+  "Pay bills",
+];
+
+//加入範例
+for (let todo of todos) {
+  addItem(todo);
+}
+
+// 函式
+function addItem(text) {
+  let newItem = document.createElement("li");
+  newItem.innerHTML = `
+    <label for="todo">${text}</label>
+    <i class="delete fa fa-trash"></i>
+  `;
+  list.appendChild(newItem);
+}
+
+
+// 1. add new todo
+addBtn.addEventListener("click", function () {
+  let inputValue = input.value;
+  //如果使用者有輸入內容，才把內容人加到list
+  if (inputValue.length > 0) {
+    addItem(inputValue);
+  }
+});
+
+// 2.刪除 todo
+list.addEventListener("click", function (event) {
+  let target = event.target;
+  if (target.classList.contains("delete")) {
+    //刪除垃圾桶上層的元素
+    let parentElement = target.parentElement;
+    parentElement.remove();
+  }
+  //3. 切換做完與否
+  if (target.tagName === "LABEL") {
+    target.classList.toggle("checked");
+  }
+});
+
+
